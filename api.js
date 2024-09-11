de5d8e7a40f5549e837c33fa74f543682f3bfef4
LakeNichts <jorgerojas222@hotmail.com>
2024-09-10 21:57:31 -0600

    test db

--- api.js
+++ api.js
@@ -1,15 +1,21 @@
-const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 3000;
+import express from 'express'
+import {config} from 'dotenv'
+import pg from 'pg'
 
-app.use(express.json());
+config()
 
-// Ruta básica
+const app = express()
+const pool = new pg.Pool({
+  connectionString: process.env.DATABASE_URL
+})
+
 app.get('/', (req, res) => {
-  res.send('Hello, world!');
-});
+  res.send('Hola!')
+})
 
-// Inicia el servidor
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+app.get('/ping',async (req, res) => {
+  const result = await pool.query('SELECT NOW()')
+  return res.json(result.rows[0])
+})
+
+app.listen(3000)
