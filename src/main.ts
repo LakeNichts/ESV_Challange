3a509130b6c61692d1237d780a1bb5234b5803b9
LakeNichts <jorgerojas222@hotmail.com>
2024-09-12 13:28:24 -0600

    coors

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,6 +14,14 @@ async function bootstrap() {
       transform:true,
     }),
   );
+
+  const corsOptions: CorsOptions = {
+    origin: process.env.ORIGIN_COORS, 
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  };
+
+  app.enableCors(corsOptions); // Habilita CORS con las opciones definidas
 
   await app.listen(parseInt(process.env.PORT)|| 3000);
 }
